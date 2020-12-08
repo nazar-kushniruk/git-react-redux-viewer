96fdc671c201116cb99aa88c58e2bdccf0921b42
nazar-kushniruk <kushniruknazar@gmail.com>
2020-12-09 00:03:44 +0200

    add templates
    add templates

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,54 @@ const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill", "./src/index.jsx"],
     output: {
         path: path.resolve(__dirname,"dist"),
-        filename: "[name].bundle.js"
+        filename: "[name].[hash].js"
+    },
+    devServer: {
+        port: 3000,
+        open: true 
     },
     plugins: [
         new HTMLWebpackPlugin({
             title: 'webpack Boilerplate',
-            template: "./src/index.html"})
-        //new CleanWebpackPlugin({})
+            template: "./src/index.html"}),
+            new CleanWebpackPlugin({})
     ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+      },
     module: {
         rules: [
             {
                 test: /\.(css|less)$/,
                 use: ["style-loader", "css-loader", "less-loader"] 
-            }
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)/,
+                use: ["file-loader"]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              },
+              {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-react', '@babel/preset-env']
+                  }
+                }
+              }
         ]
     }
 }
