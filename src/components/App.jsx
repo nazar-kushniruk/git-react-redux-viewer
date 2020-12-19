811704d61121b8c216c16f5a4f7d513baf462225
nazar-kushniruk <kushniruknazar@gmail.com>
2020-12-19 03:00:55 +0200

    game with reducers
    game with reducers

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {setCount} from '../reducers/reposReducer'
 import './app.less';
 
 const App = () =>  {
+    const dispatch = useDispatch(); 
+    const count = useSelector(state => state.count)
+
+
+    function onCountClick () {
+        dispatch(setCount(5));
+    } 
+
+    
     return (
         <div className="app">
-            React is working!!!
+            <button onClick={()=>onCountClick()}>
+                COUNT
+            </button>
+            <br/>
+            <div>{count}</div>
         </div>
     );
 }; 
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,13 @@ import './index.less';
 import React from  'react';
 import {render} from 'react-dom';
 import App from './components/App';
+import {Provider} from 'react-redux';
+import {store} from './reducers'
+ 
+render (
+        <Provider store={store}>
+            <App/>
+        </Provider>,
+        document.getElementById('root')
+);
 
-render(
-    <App/>,
-    document.getElementById('root')
-    );
